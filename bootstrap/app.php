2456cdf4babfc9873131528578974a4fd3ddf724
Tristan <ricemaking69@gmail.com>
2025-05-08 09:51:25 -0500

    maybe it works now?

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(
-            at: ['*'], 
-            headers: Illuminate\Http\Request::HEADER_X_FORWARDED_FOR | Illuminate\Http\Request::HEADER_X_FORWARDED_HOST
-        );
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
