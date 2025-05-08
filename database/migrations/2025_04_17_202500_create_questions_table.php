edc520ff53eba04c2204640ae72bea557b970e6a
Tristan Nono <ricemaking69@gmail.com>
2025-05-08 15:51:49 -0500

    the fix?

--- database/migrations/2025_04_17_202500_create_questions_table.php
+++ database/migrations/2025_04_17_202500_create_questions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('quiz_id')->constrained()->onDelete('cascade')->onDelete('cascade');
+            $table->foreignId('quiz_id')->constrained()->onDelete('cascade');
             $table->text('question');
             $table->timestamps();
         });
