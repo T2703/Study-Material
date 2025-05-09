d74eb36512b9cc3f4d2466261e265b5f0981b09a
Tristan <ricemaking69@gmail.com>
2025-05-09 10:25:23 -0500

    URL & practices

--- database/migrations/2025_04_21_154528_create_flashcards_table.php
+++ database/migrations/2025_04_21_154528_create_flashcards_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('flashcards', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('flashcard_set_id')->constrained()->onDelete('cascade');
+            $table->foreignId('flashcard_set_id')->constrained('flashcard_sets')->onDelete('cascade');
             $table->text('question');
             $table->text('answer');
             $table->timestamps();
--- database/migrations/2025_04_23_142853_create_favorites_table.php
+++ database/migrations/2025_04_23_142853_create_favorites_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->morphs('favoritable');
             $table->timestamps();
         });
