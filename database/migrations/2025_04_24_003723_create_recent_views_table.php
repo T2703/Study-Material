ecaeb8a420b513ff82938dfe843c41a2df0c6ae1
Tristan <ricemaking69@gmail.com>
2025-05-09 10:51:14 -0500

    try this again

--- database/migrations/2025_04_24_003723_create_recent_views_table.php
+++ database/migrations/2025_04_24_003723_create_recent_views_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('recent_views', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id') ->constrained('users');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->morphs('viewable'); 
             $table->timestamps();
         });
