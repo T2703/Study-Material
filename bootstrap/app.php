99bb7ed4ec8b6220225fb26b22c6628db91aefb9
Tristan Nono <ricemaking69@gmail.com>
2025-05-09 16:15:02 -0500

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(at: '*');
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                     Request::HEADER_X_FORWARDED_HOST |
+                     Request::HEADER_X_FORWARDED_PORT |
+                     Request::HEADER_X_FORWARDED_PROTO
+        );
+        
+        
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+
     })->create();
